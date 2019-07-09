f65a854b4f364b428440c77de108c910245d1fe4
Constantin CHTANKO <constantin.chtanko@etu.formation-aftec.com>
2019-07-10 01:14:16 +0200

    Requete qui remonte les informations concernant le ticket

--- AGTPPE/Controllers/HomeController.cs
+++ AGTPPE/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,7 +10,7 @@ namespace AGTPPE.Controllers
 {
     public class HomeController : Controller
     {
-        
+
         public ActionResult Index()
         {
             return View();
@@ -36,8 +38,50 @@ namespace AGTPPE.Controllers
         public ActionResult IncidentsAdmin()
         {
             return View();
+
         }
 
+        
+        public ActionResult IncidentsOuverts(object sender, EventArgs e)
+        {
+           
 
+       
+            string connetionString;
+            SqlConnection cnn;
+
+            connetionString = @"Data Source=DESKTOP-G18P8BC\SQLEXPRESS;Initial Catalog=KNInfo;Integrated Security=True";
+
+            cnn = new SqlConnection(connetionString);
+
+            cnn.Open();
+
+           // Response.Write("Connexion établie");
+            //cnn.Close();
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = " ";
+
+            sql = "Select emplacementMaterielTicket,descriptionIncident, dateCreationTicket from TICKETS WHERE dateCreationTicket IS NOT NULL";
+
+            command = new SqlCommand(sql, cnn);
+
+            dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+               
+                Output = Output + dataReader.GetValue(0) + " | " + dataReader.GetValue(1) + " Ouvert à : "+dataReader.GetValue(2)+ "</br>";
+            }
+
+            Response.Write(Output);
+            dataReader.Close();
+            command.Dispose();
+            cnn.Close();
+
+            return View();
+
+        }
+    
     }
 }
